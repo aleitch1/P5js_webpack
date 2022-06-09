27d6de5b9dddecf19f2b6da2a34a03ee992d7ee4
pretentiousgit <alex.leitch@gmail.com>
2022-06-09 17:32:19 -0400

    remove font-lock and tweak main/

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-param-reassign */
 import * as P5 from './vendor/p5.min';
 
-// Fontswapper
-import { fontSwapper } from './library/fontSwapper';
-
 // P5 Dependencies
 import buildingGenerator from './p5components/buildingGenerator';
 import storyGenerator from './p5components/storyGenerator';
@@ -79,5 +76,3 @@ const s = (p5Sketch) => {
 };
 
 const p5 = new P5(s);
-
-fontSwapper();
