d3c647cb51263285bbd25ef86c5686bd3bff5a50
Alex Leitch <aleitch1@umd.edu>
2023-12-29 10:38:16 -0500

    Move indent on A/C, change fill to gray

--- src/p5components/drawAirConditioner.js
+++ src/p5components/drawAirConditioner.js
@@ -15,22 +15,44 @@ function drawAirConditioner(config) {
   // plus whether it has an ac or not
   // needs to be internal to that window.
 
-  const w = acWidth;
+  // const w = acWidth;
   const xCorrect = x - acWidth / 2;
-  const baseHeight = w * 0.618;
-  const yCorrect = getBool() ? windowHeight + y - baseHeight : y;
+  const boxHeight = acWidth * 0.618; // clever, constrained by window width?
+  const yCorrect = getBool() ? windowHeight + y - boxHeight : y;
 
+  // Set the stroke to black and the fill to white
   p5Sketch.stroke(0, 0, 0);
   p5Sketch.fill(255, 255, 255);
+
   // an a/c is always the same width.x
-  p5Sketch.rect(xCorrect, yCorrect, w, baseHeight);
-  p5Sketch.rect(xCorrect + 3, yCorrect + 3, w - 6, (baseHeight) - 6);
+  // TODO: make this scale by screen size?
 
-  p5Sketch.rect(xCorrect + 3, yCorrect + 5, w - 6, (baseHeight) - 10);
-  p5Sketch.rect(xCorrect + 3, yCorrect + 7, w - 6, (baseHeight) - 14);
-  p5Sketch.rect(xCorrect + 3, yCorrect + 9, w - 6, (baseHeight) - 18);
+  // outer case of a/c
+  p5Sketch.rect(xCorrect, yCorrect, acWidth, boxHeight);
 
-  p5Sketch.rect(xCorrect + (w / 2) - 4, yCorrect + (baseHeight) - 2, 8, (baseHeight) - 24);
+  // inner case of a/c
+  p5Sketch.fill(150, 150, 150);
+  p5Sketch.rect(xCorrect + 3, yCorrect + 3, acWidth - 6, (boxHeight) - 6);
+  p5Sketch.fill(255, 255, 255);
+
+  // inner a/c, replace with 1 rect full of dots?
+  // p5Sketch.rect(xCorrect + 3, yCorrect + 5, w - 6, (boxHeight) - 10);
+  // p5Sketch.rect(xCorrect + 3, yCorrect + 7, w - 6, (boxHeight) - 14);
+  // p5Sketch.rect(xCorrect + 3, yCorrect + 9, w - 6, (boxHeight) - 18);
+
+  // little weird dip in a-c line, which should just be white
+  p5Sketch.stroke(255, 255, 255);
+
+  const indent = {
+    // vertPosition: yCorrect + (boxHeight) - 2,
+    horzPosition: xCorrect + (acWidth / 2) - 4,
+    vertPosition: yCorrect + 1,
+    width: acWidth / 4, // not acWidth, apparently!
+    height: 3 // this is a position?
+    // height: (boxHeight) - 24 // this is a position?
+  };
+
+  p5Sketch.rect(indent.horzPosition, indent.vertPosition, indent.width, indent.height);
   p5Sketch.stroke(0, 0, 0);
   p5Sketch.noFill();
 }
